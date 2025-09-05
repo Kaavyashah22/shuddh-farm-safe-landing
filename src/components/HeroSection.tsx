eb6e01846ac5996fb110993fc756ff3204bb1c0b
Kaavyashah22 <kaavyashah153@gmail.com>
2025-09-05 19:52:29 +0530

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -32,7 +32,7 @@ const HeroSection = () => {
               <ArrowRight className="ml-2 w-4 h-4 transition-transform group-hover:translate-x-1" />
             </Button>
             <Button variant="outline" size="lg">
-              Download App
+              Pharmacy Poral
             </Button>
           </div>
 
